0ad8fedb0db0b26f0e6a38bb463d0034fe3af792
Admin <admin@MacBook-Air.home>
2017-12-11 07:27:28 -0500

    More islands + rock placing

---
+++ Assets/Scripts/PlaceRocks.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceRocks : MonoBehaviour {
+
+	public GameObject rock;
+	public float numRocks;
+	public float maxDistance;
+	public SimulateObjects s;
+
+	List<GameObject> rocks = new List<GameObject> (0);
+
+	[ContextMenu("Place and Simulate")]
+	public void PlaceAndSimulate () {
+		if (rock == null || maxDistance == 0 || numRocks == 0 || s == null) {
+			Debug.LogError ("Unable to place any rocks.");
+			return;
+		}
+		for (int i = 0; i < numRocks; i++) {
+			Vector2 random = Random.insideUnitCircle * maxDistance;
+			Vector3 pos = new Vector3 (random.x, 50f, random.y);
+			GameObject newRock = Instantiate (rock, pos, Quaternion.identity, s.transform) as GameObject;
+			rocks.Add (newRock);
+		}
+		s.Simulate ();
+
+		foreach (GameObject r in rocks) {
+			if (r.transform.position.y < -50) {
+				DestroyImmediate (r);
+			}
+		}
+	}
+
+	[ContextMenu("Remove Rocks")]
+	public void ResetRocks () {
+		foreach (GameObject r in rocks) {
+			DestroyImmediate (r);
+		}
+		rocks = new List<GameObject> (0);
+	}
+}
---
+++ Assets/Scripts/SimulateObjects.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SimulateObjects : MonoBehaviour {
+
+	static int maxIterations = 1000;
+	SimulatedBody[] simulatedBodies;
+
+	[ContextMenu("Run Simulation")]
+	public void Simulate () {
+
+		Physics.autoSimulation = false;
+
+		simulatedBodies = FindObjectsOfType<Rigidbody> ().Select (rb => new SimulatedBody (rb, rb.transform.IsChildOf (this.transform))).ToArray ();
+		for (int i = 0; i < maxIterations; i++) {
+			Physics.Simulate (Time.fixedDeltaTime);
+			if (simulatedBodies.All (body => body.rigidbody.IsSleeping ())) {
+				print (i);
+				break;
+			}
+		}
+
+		Physics.autoSimulation = true;
+
+		foreach (SimulatedBody body in simulatedBodies) {
+			if (!body.isChild) {
+				body.Reset ();
+			}
+		}
+	}
+
+	[ContextMenu("Reset Simulation")]
+	public void ResetAllBodies () {
+		if (simulatedBodies != null) {
+			foreach (SimulatedBody body in simulatedBodies) {
+				body.Reset ();
+			}
+		}
+	}
+
+	struct SimulatedBody {
+		public readonly Rigidbody rigidbody;
+		public readonly bool isChild;
+		readonly Vector3 originalPosition;
+		readonly Quaternion originalRotation;
+		readonly Transform transform;
+
+		public SimulatedBody (Rigidbody rigidbody, bool isChild) {
+			this.rigidbody = rigidbody;
+			this.isChild = isChild;
+			transform = rigidbody.transform;
+			originalPosition = transform.position;
+			originalRotation = transform.rotation;
+		}
+
+		public void Reset () {
+			transform.position = originalPosition;
+			transform.rotation = originalRotation;
+			if (rigidbody != null) {
+				rigidbody.velocity = Vector3.zero;
+				rigidbody.angularVelocity = Vector3.zero;
+			}
+		}
+	}
+}

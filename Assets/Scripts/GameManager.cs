078a1a6770855ebec6c84d1a767f9b7b91fb04dd
Admin <admin@MacBook-Air.home>
2017-12-10 22:40:50 -0500

    First scene + floating island mesh

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public static void LoadLevel (int level) {
+		SceneManager.LoadScene (level);
+		// Build index correlates directly with level number: 0 = main menu, 1 = level 1, 2 = level 2 and so on
+	}
+}

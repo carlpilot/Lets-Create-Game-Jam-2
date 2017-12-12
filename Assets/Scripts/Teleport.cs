d27dcbaadb4ce9170d97b99b27827b6410efb207
Admin <admin@MacBook-Air.home>
2017-12-11 19:38:52 -0500

    teleporter things

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+
+	public GameObject teleporter;
+	Vector3 lastPosition;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			RaycastHit hit;
+			if (Physics.Raycast (Camera.main.transform.position, Camera.main.transform.forward, out hit)) {
+				if (hit.transform.tag == "Teleporter") {
+					lastPosition = transform.position;
+					transform.position = hit.transform.position + Vector3.up;
+					if (!hit.transform.GetComponent<Teleporter> ().isLastPositionMarker) {
+						foreach (Teleporter t in FindObjectsOfType<Teleporter>()) {
+							t.teleporterActive = false;
+						}
+						GameObject newTeleporter = Instantiate (teleporter, lastPosition, Quaternion.identity) as GameObject;
+						newTeleporter.GetComponent<Teleporter>().isLastPositionMarker = true;
+					} else {
+						Destroy (hit.transform.gameObject);
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+
+	public Light light;
+	public ParticleSystem particleSystem;
+	public bool teleporterActive = true;
+	public bool isLastPositionMarker = false;
+
+	void Awake () {
+		if (light == null) {
+			light = GetComponentInChildren<Light> ();
+			particleSystem = GetComponent<ParticleSystem> ();
+		}
+	}
+
+	void Update () {
+		if (teleporterActive) {
+			light.enabled = true;
+			if (!particleSystem.isPlaying) {
+				particleSystem.Play ();
+			}
+		} else {
+			light.enabled = false;
+			if (particleSystem.isPlaying) {
+				particleSystem.Stop ();
+			}
+		}
+	}
+}

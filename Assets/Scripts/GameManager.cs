fe6b4e009b16e520efd5439f3ebea3709a1c750c
Admin <admin@MacBook-Air.home>
2017-12-15 08:05:26 -0500

    Win menu made
    Made a functional win menu -- not yet implemented displaying the menu upon game win

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,9 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
+	public static GameManager instance;
 	GameObject player;
+	public GameObject winMenu;
+	public GameObject loseMenu;
 
 	void Start () {
+		instance = this;
 		player = GameObject.FindGameObjectWithTag ("Player");
 	}
 
@@ -16,13 +20,31 @@ public class GameManager : MonoBehaviour {
 		// Build index correlates directly with level number: 0 = main menu, 1 = level 1, 2 = level 2 and so on
 	}
 
+	public static void ReloadLevel () {
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+	}
+
+	public static void MainMenu () {
+		LoadLevel (0);
+	}
+
+	public static void LevelSelection () {
+		SceneManager.LoadScene ("levelselection");
+	}
+
 	void Update () {
 		if (player.transform.position.y < -30f || player.transform.position.sqrMagnitude > 10000) {
 			EndGame ();
 		}
 	}
 
-	void EndGame () {
+	public void EndGame () {
 		player.GetComponent<Rigidbody> ().isKinematic = true;
+		Cursor.lockState = CursorLockMode.None;
+		Cursor.visible = true;
+	}
+
+	public void WinGame () {
+		PlayerPrefs.SetInt ("HasWonLevel" + SceneManager.GetActiveScene ().buildIndex, 1);
 	}
 }

3bc7af3451f565d67856ff037876c6369a758fb4
Admin <admin@MacBook-Air.home>
2017-12-16 15:11:23 -0500

    Level selection menu
    Added the level selection menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,7 +16,7 @@ public class GameManager : MonoBehaviour {
 		player = GameObject.FindGameObjectWithTag ("Player");
 	}
 
-	public static void LoadLevel (int level) {
+	public void LoadLevel (int level) {
 		SceneManager.LoadScene (level);
 		// Build index correlates directly with level number: 0 = main menu, 1 = level 1, 2 = level 2 and so on
 	}

066d22500c9d09742b7e596342dab08161e7d882
Admin <admin@MacBook-Air.home>
2017-12-21 13:11:35 -0500

    Lev 1 high scores done

--- Assets/Scripts/HighScores/HighScoreDisplay.cs
+++ Assets/Scripts/HighScores/HighScoreDisplay.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HighScoreDisplay : MonoBehaviour {
 
 	public GameObject scoreItemPrefab;
 	public GameObject scrollViewContent;
+	public GameObject yourScore;
+	public Timer timer;
 
 	public void ShowScores (WWW www) {
 		StartCoroutine (showScores (www));
@@ -15,7 +18,47 @@ public class HighScoreDisplay : MonoBehaviour {
 		while (!www.isDone) {
 			yield return null;
 		}
-		//print (www.text);
+
+		DisplayScores (www.text);
+
+		yield return new WaitForSeconds (1f);
+
 		StartCoroutine (showScores (new WWW (www.url)));
+	}
+
+	void DisplayScores (string input) {
+
+		for (int i = 0; i < scrollViewContent.transform.childCount; i++) {
+			Destroy (scrollViewContent.transform.GetChild (i).gameObject);
+		}
+
+		yourScore.transform.Find ("PlayerName").GetComponent<Text> ().text = PlayerPrefs.GetString ("PlayerUsername");
+		yourScore.transform.Find ("Time").GetComponent<Text> ().text = timer.time + " sec";
+
+		string[] items = input.Split ('\n');
+		for (int i = 0; i < items.Length; i++) {
+			if (items [i].Length > 2) {
+
+				string[] subItems = items [i].Split ('|');
+
+				if (subItems.Length > 2) {
+
+					string username = subItems [0];
+					int score = int.Parse (subItems [1]);
+					float time = HighScores.ScoreToTime (score);
+
+					float y = -(scoreItemPrefab.GetComponent<RectTransform> ().rect.height / 2f + i * scoreItemPrefab.GetComponent<RectTransform> ().rect.height);
+
+					GameObject newItem = Instantiate (scoreItemPrefab);
+					newItem.transform.SetParent (scrollViewContent.transform, false);
+					newItem.GetComponent<RectTransform> ().anchoredPosition = new Vector3 (0f, y, 0f);
+
+					newItem.transform.Find ("PlayerName").GetComponent<Text> ().text = username;
+					newItem.transform.Find ("Time").GetComponent<Text> ().text = time + " sec";
+
+				}
+
+			}
+		}
 	}
 }
--- Assets/Scripts/HighScores/HighScores.cs
+++ Assets/Scripts/HighScores/HighScores.cs
@@ -6,7 +6,7 @@ public class HighScores : MonoBehaviour {
 
 	public static void SaveScore (int level, string player, float time) {
 		// Scores of 1 are used since time is the main scoring factors
-		string url = "http://dreamlo.com/lb/" + SecretCode.Private (level) + "/add/" + WWW.EscapeURL (player) + "/-" + (100000 - Mathf.RoundToInt(time * 1000f));
+		string url = "http://dreamlo.com/lb/" + SecretCode.Private (level) + "/add/" + WWW.EscapeURL (player) + "/" + (100000 - Mathf.RoundToInt(time * 100f));
 		print (url);
 		new WWW (url);
 	}
@@ -16,7 +16,7 @@ public class HighScores : MonoBehaviour {
 	}
 
 	public static WWW GetScores (int level) {
-		string url = "http://dreamlo.com/lb/" + SecretCode.Public (level) + "/pipe-asc/20";
+		string url = "http://dreamlo.com/lb/" + SecretCode.Public (level) + "/pipe/20";
 		return new WWW (url);
 	}
 
@@ -28,5 +28,9 @@ public class HighScores : MonoBehaviour {
 	public static void Clear (int level) {
 		string url = "http://dreamlo.com/lb/" + SecretCode.Private (level) + "/clear";
 		new WWW (url);
+	}
+
+	public static float ScoreToTime (int score) {
+		return (100000f - (float)score) / 100f;
 	}
 }
